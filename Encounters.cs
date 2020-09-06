f926b8e60ccbd74212b1253e312328dc4ee8dc9a
Gaven Wright <gavenlwright@hotmail.com>
2020-09-06 16:17:23 -0700

    first commit

---
+++ Encounters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_Based_Assement
+{
+    class Encounters
+    {
+        //Encounter Generic
+
+
+        // Encounters 
+       public static void FirstEncounter()
+        {
+            Console.WriteLine("You throw open the door and grab a rusty metal sword while charging toward the captor");
+            Console.WriteLine("He turns....");
+            Console.ReadKey();
+        }
+
+
+        // Encounter Tools
+        public static void Combat(bool random, string name, int power, int health)
+        {
+            string n = "";
+            int p = 0;
+            int h = 0;
+            if(random)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_Based_Assement
+{
+    class Player
+    {
+        public string name;
+        public int coins = 0;
+        public int health = 10;
+        public int armorValue = 0;
+        public int potion = 5;
+    }
+}
