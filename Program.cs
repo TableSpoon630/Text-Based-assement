a80e26a355af7511abe89ef5a9593e199cf4808a
Gaven Wright <gavenlwright@hotmail.com>
2020-09-06 16:20:27 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Text_Based_Assement
+{
+    class Program
+    {
+        public static Player currentPlayer = new Player();
+       static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("The Dungeon!");
+            Console.WriteLine("Name: ");
+            currentPlayer.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("You awake in a cold room. You feel tired,hungry and you are having trouble remembering");
+            Console.WriteLine("anything about your past. ");
+            if (currentPlayer.name == "")
+                Console.WriteLine("You can't even remember your own name....");
+            else
+                Console.WriteLine("You know your name is " + currentPlayer.name);
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("You grope around in the pitch black until you find a door handle. You feel some resistance as");
+            Console.WriteLine("you turn the handle, but the rusty lock breaks with little effort. You see your captor");
+            Console.WriteLine("standing with his back to you outside the door.");
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
